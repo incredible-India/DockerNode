9dd80ec3a56aa51001df703162db7cf01c9d6f7f
Himanshu kumar Sharma <SharmaHi@incadeadev.com>
2024-08-20 20:03:24 +0530

    added logs file and volume in Docker file

--- dockerfile
+++ dockerfile
@@ -1,4 +1,5 @@
 FROM node:latest
+WORKDIR /app
 COPY . .
 RUN npm i
 EXPOSE 80
--- index.js
+++ index.js
@@ -1,16 +1,26 @@
 const express =  require("express");
-
+const fs = require("fs");
 const app = express();
 
 app.get("/",(req,res)=>{
 
-
-    return res.json({
-        "name":"Himanshu"
-    });
-
+for (let index = 0; index < 5; index++) {
+    fs.appendFileSync("./logs.txt",`Hi ${index}\n`);
+    
+}
+ return res.send("Hi file has been updated")
 
 });
+
+app.get("/read",(req,res)=>{
+
+   var a = fs.readFileSync("./logs.txt","utf-8");
+   console.log(a);
+   
+     return res.send(a)
+    
+    });
+    
 
 
 app.listen(8000,()=>{
